ad5f7806ad04195b07345d9b1ce7e1f50ea2995c
KhafiaAyyub <khafiaayyub@gmail.com>
2025-05-23 14:01:28 +0530

    commit

---
+++ src/main/java/com/example/travel/BooknGo/BooknGoApplication.java
@@ -0,0 +1,13 @@
+package com.example.travel.BooknGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooknGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooknGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/travel/BooknGo/BooknGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travel.BooknGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooknGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
